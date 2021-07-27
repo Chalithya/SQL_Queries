7eaee130e47b57e7050b4d55d35203b1bdd21e0c
Chalithya Sangeeth <chalithyaofficial@gmail.com>
2021-07-27 08:52:01 +0530

    example DB created

---
+++ Sample_DB_Creation.sql
@@ -0,0 +1,53 @@
+--CREATE DATABASE testDB;
+
+USE testDB;
+
+
+CREATE TABLE Student(
+	Sno		char(9)			NOT NULL,
+	Sname	varchar(20)		NULL,
+	Age		int				Null
+	);
+
+
+CREATE UNIQUE INDEX Student_ndx ON Student(Sno)	
+
+
+
+CREATE TABLE Course(
+	Cno			char(9)			NOT NULL,
+	Title		varchar(20)		NULL,
+	Credits		int				Null
+	);
+
+CREATE UNIQUE INDEX Course_ndx ON Course(Cno)	
+
+
+CREATE TABLE Professor(
+	Fname		varchar(20)		NOT NULL,
+	Lname		varchar(20)		NULL,
+	Dept		varchar(20)		NULL,
+	Rank		varchar(20)		NULL,
+	Salary		varchar(20)		NULL,
+	Age			int				Null
+	);
+
+--DROP TABLE Professor;
+CREATE UNIQUE INDEX Prof_name ON Professor(Fname, Lname)
+
+
+CREATE TABLE Take(
+	Sno		char(9)			NOT NULL,
+	Cno		char(9)			NOT NULL,
+	Grade	varchar(2)		NULL
+	);
+
+CREATE TABLE Teach(
+	Fnmae	varchar(20)		NOT NULL,
+	Lname	varchar(20)		NULL,
+	Cno		char(9)			NOT NULL	
+	);
+
+
+
+
